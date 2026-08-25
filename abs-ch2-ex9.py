7f2682c289368d8ab4ce20bc94f2d4b3a670abe6
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-08-24 22:34:15 -0400

    Provide question

--- abs-ch2-ex9.py
+++ abs-ch2-ex9.py
@@ -1,3 +1,5 @@
+# 9.  Write code that prints Hello if 1 is stored in spam, prints Howdy if 2 is stored in spam, and prints Greetings! if anything else is stored in spam.
+
 print('How much spam would you like today?')
 spam = input('>')
 if spam == '1':
