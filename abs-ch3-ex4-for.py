abbada974a268f894c40eaa2de8559cc4d3efe1e
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-08-31 10:43:24 -0400

    Complete exercises in chapter 3

---
+++ abs-ch3-ex4-for.py
@@ -0,0 +1,4 @@
+# 4.  Write a short program that prints the numbers 1 to 10 using a for loop. Then, write an equivalent program that prints the numbers 1 to 10 using a while loop. This is the for example which uses the builtin range function.
+
+for i in range(1,11,1):
+    print (i)
---
+++ abs-ch3-ex4-while.py
@@ -0,0 +1,6 @@
+# 4.  Write a short program that prints the numbers 1 to 10 using a for loop. Then, write an equivalent program that prints the numbers 1 to 10 using a while loop. This is the while example which uses a C-style counter.
+
+i = 1
+while i < 11:
+    print(i)
+    i = i + 1
