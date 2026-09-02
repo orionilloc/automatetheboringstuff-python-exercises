594792a8351cb746772f5dbf7459cc129eae9148
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-09-02 13:05:50 -0400

    Provide first solution while using end instead of sep

---
+++ ch4-functions/abs-ch4-ex2-collatz-input-validation.py
@@ -0,0 +1,28 @@
+def collatz(number):
+    if int(number) % 2 == 0: # if even
+        result = int(number) // 2
+    else: # if odd
+        result = int(number) * 3 + 1
+    print(result, end=' ')
+    return result
+
+number = input('Please enter a number:')
+
+while True:
+    try:
+        number = int(number)
+        if number == 0:
+            print('Invalid input provided.')
+            number = input('Please enter a non-zero number:')
+        else:
+            break
+    except ValueError:
+        print('Invalid input provided.')
+        number = input('Please enter a non-zero number:')
+
+while int(number) != 1:
+    try:
+        number = collatz(number)
+    except ValueError:
+        print('Invalid input provided.')
+        number = input('Please enter a non-zero number:')
