140133045fba9734c2ab720608a4042393882d26
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-09-04 10:27:04 -0400

    Wrap input into integer function call to cut redundant lines

--- other-exercises/guessinggamev2.py
+++ other-exercises/guessinggamev2.py
@@ -4,17 +4,14 @@ random_number = random.randint(1,50)
 
 def guessing_sequence(user_guess):
     if int(user_guess) > int(random_number):
-        user_guess = input('User-provided guess is greater than the random number. Pick a lower number: ')
-        user_guess = int(user_guess)
+        user_guess = int(input('User-provided guess is greater than the random number. Pick a lower number: '))
     elif int(user_guess) < int(random_number):
-        user_guess = input('User-provided guess is lower than the random number. Pick a higher number: ')
-        user_guess = int(user_guess)
+        user_guess = int(input('User-provided guess is lower than the random number. Pick a higher number: '))
     return int(user_guess)
 
-user_guess = input('Please enter a random number between 1 and 50: ')
-user_guess = int(user_guess)
+user_guess = int(input('Please enter a random number between 1 and 50: '))
 
 while user_guess != random_number:
     user_guess = guessing_sequence(user_guess)
 
-print(f'You have guessed the correct answer of {random_number}. Congrats!')
+print(f'You have guessed the correct random number: {random_number}. Congratulations!')
