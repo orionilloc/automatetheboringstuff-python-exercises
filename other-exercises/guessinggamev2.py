3b7820b7ef5b6aad5f2e2dc950fbabdfc8a3566c
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-09-04 13:02:29 -0400

    Add input validation for non-numerical guesses

--- other-exercises/guessinggamev2.py
+++ other-exercises/guessinggamev2.py
@@ -3,13 +3,22 @@ import random
 random_number = random.randint(1,50)
 
 def guessing_sequence(user_guess):
-    if user_guess > random_number:
-        user_guess = int(input('User-provided guess is greater than the random number. Pick a lower number: '))
-    elif user_guess < random_number:
-        user_guess = int(input('User-provided guess is lower than the random number. Pick a higher number: '))
-    return user_guess
+    while True:
+        try:
+            if user_guess > random_number:
+                user_guess = int(input('User-provided guess is greater than the random number. Pick a lower number: '))
+            elif user_guess < random_number:
+                user_guess = int(input('User-provided guess is lower than the random number. Pick a higher number: '))
+            return user_guess
+        except ValueError:
+            print('Invalid input provided.')
 
-user_guess = int(input('Please enter a random number between 1 and 50: '))
+while True:
+    try:
+        user_guess = int(input('Please enter a random number between 1 and 50: '))
+        break
+    except ValueError:
+        print('Invalid input provided.')
 
 while user_guess != random_number:
     user_guess = guessing_sequence(user_guess)
