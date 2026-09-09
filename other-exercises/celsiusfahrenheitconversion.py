e0d2d79c3d22f89d912313b64649d39b4b566336
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-09-08 22:13:26 -0400

    Create pseudocode rough draft and skeletal structure

---
+++ other-exercises/celsiusfahrenheitconversion.py
@@ -0,0 +1,38 @@
+# celsius to fahrenheit
+# fahrenheitto celsius
+
+# number conversion
+
+#input validation (ensuring that its F or C - then have a portion split to be the number conversion)o
+
+# user_temperature
+# temperature_scale
+#then output
+# need a temperature conversion function in here as well- but how much can be nested in here
+#def temperature_conversion():
+#    if user_temperature_scale == str('F'):
+#        do some math
+#    else:
+#        do some math to convert from celsius to fahrenheit
+# need actual math sequencing in here as well
+
+# see below- have i essentially made a use pane and how should this be formatted here
+
+while True:
+    print("Enter a temperature and temperature scale to convert from: ")
+    print("Example user input: 84 F")
+    print("Example temperature conversion: decideondecimals C")
+    print("")
+    input("Enter a temperature and temperature scale to convert from: ")
+    break
+
+
+#print(f' {user_temperature_number} {user_temperature_scale} converts to {converted_temperature_number} {converted_temperature_scale}. Very cool!')
+
+
+# (32F − 32) × 5/9 = 0°C
+#
+# (0°C × 9/5) + 32 = 32°F
+# include try and except somewhere in here
+
+# store first field as integer - second field as F or C (cant be any other one here)
