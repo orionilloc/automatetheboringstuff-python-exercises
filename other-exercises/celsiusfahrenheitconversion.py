801978d17c6f83d46138dec7eea9c4bdb105f9ba
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-09-09 21:19:45 -0400

    Provide core logic for switching between celsius/fahrenheit based on user input; retain stale while logic for later development

--- other-exercises/celsiusfahrenheitconversion.py
+++ other-exercises/celsiusfahrenheitconversion.py
@@ -1,23 +1,3 @@
-# celsius to fahrenheit
-# fahrenheitto celsius
-
-# number conversion
-
-#input validation (ensuring that its F or C - then have a portion split to be the number conversion)o
-
-# user_temperature
-# temperature_scale
-#then output
-# need a temperature conversion function in here as well- but how much can be nested in here
-#def temperature_conversion():
-#    if user_temperature_scale == str('F'):
-#        do some math
-#    else:
-#        do some math to convert from celsius to fahrenheit
-# need actual math sequencing in here as well
-
-# see below- have i essentially made a use pane and how should this be formatted here
-
 while True:
     print("Enter a temperature and temperature scale to convert from: ")
     print("Example user input: 84 F")
@@ -26,22 +6,11 @@ while True:
     user_input = input("Enter a temperature and temperature scale to convert from: ")
     user_temperature_number, user_temperature_scale = user_input.split()
 
-    converted_temperature_number_to_celsius = (int(user_temperature_number) -32) * 5 / 9
-    print(converted_temperature_number_to_celsius)
-    converted_temperature_number_to_fahrenheit = (int(user_temperature_number) * 9 / 5 ) + 32
-    print(converted_temperature_number_to_fahrenheit )
+    if user_temperature_scale in ['F', 'f']:
+        converted_temperature_number_to_celsius = (int(user_temperature_number) -32) * 5 / 9
+        print(f'{converted_temperature_number_to_celsius} C')
+
+    elif user_temperature_scale in ['C', 'c']:
+        converted_temperature_number_to_fahrenheit = (int(user_temperature_number) * 9 / 5 ) + 32
+        print(f'{converted_temperature_number_to_fahrenheit} F')
     break
-
-
-#print(f' {user_temperature_number} {user_temperature_scale} converts to {converted_temperature_number} {converted_temperature_scale}. Very cool!')
-#try:
-#if:
-    #second value is f? then convert to c
-#else:
-#except
-#
-# (0°C × 9/5) + 32 = 32°F
-# include try and except somewhere in here
-#
-# store first field as integer - second field as F or C (cant be any other one here)
-# next version would have more input validation or possibly something else more fun
