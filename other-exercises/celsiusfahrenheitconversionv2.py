4d97000315ffa66725b19055eacf01ef00f5c2c9
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-09-13 15:14:49 -0400

    Add graceful exit mechanism and wrapped logic in try/except clauses

---
+++ other-exercises/celsiusfahrenheitconversionv2.py
@@ -0,0 +1,25 @@
+import sys
+
+print("Example user input: 84 F")
+print("Example temperature conversion: 28.88888888888889 C")
+print("")
+
+while True:
+    try:
+        user_input = input("Enter a temperature and temperature scale to convert from: ")
+        user_temperature_number, user_temperature_scale = user_input.split()
+        if user_temperature_scale in ['F', 'f']:
+            converted_temperature_number_to_celsius = (int(user_temperature_number) -32) * 5 / 9
+            print(f'{converted_temperature_number_to_celsius} C')
+        elif user_temperature_scale in ['C', 'c']:
+            converted_temperature_number_to_fahrenheit = (int(user_temperature_number) * 9 / 5 ) + 32
+            print(f'{converted_temperature_number_to_fahrenheit} F')
+        else:
+            print("Invalid temperature scale value detected. Did you mean to write C or F for temperature scale?")
+    except ValueError as e:
+        print("")
+        print(f"Invalid input detected; {e}")
+    except KeyboardInterrupt:
+        print("")
+        print("KeyboardInterrupt detected. Exiting temperature conversion program...")
+        sys.exit(0)
