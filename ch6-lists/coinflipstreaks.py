fbcae1b496639f5bf0865b62638af23cc687c8de
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-09-18 22:26:12 -0400

    Echo book-provided template

---
+++ ch6-lists/coinflipstreaks.py
@@ -0,0 +1,8 @@
+import random
+number_of_streaks = 0
+for experiment_number in range(10000):  # Run 100,000 experiments total.
+    # Code that creates a list of 100 'heads' or 'tails' values
+
+    # Code that checks if there is a streak of 6 heads or tails in a row
+
+print('Chance of streak: %s%%' % (number_of_streaks / 100))
