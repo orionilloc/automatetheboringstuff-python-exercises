976ce695647e5cec990f21a0c9f12bd1e86522e7
orionilloc <sterne.collin.netsecjobs@gmail.com>
2026-09-19 12:32:34 -0400

    Provide logic check for streaks; troubleshoot number total variance

--- ch6-lists/coinflipstreaks.py
+++ ch6-lists/coinflipstreaks.py
@@ -1,22 +1,18 @@
 import random
 number_of_streaks = 0
+streak_H = ["H"] * 6
+streak_T = ["T"] * 6
+
 for experiment_number in range(10000):  # Run 100,000 experiments total.
+
     # Code that creates a list of 100 'heads' or 'tails' values
     coin_flip_number = 0
-    heads_tails_list = list(random.choice(["H", "T"]))
-    while coin_flip_number <= 100:
+    heads_tails_list = []
+    while coin_flip_number < 100:
         heads_tails_list.append(random.choice(["H", "T"]))
         coin_flip_number += 1
-    print(heads_tails_list)
-
 
     # Code that checks if there is a streak of 6 heads or tails in a row
-
-###print('Chance of streak: %s%%' % (number_of_streaks / 100))
-
-#ok so cant be the print function in here
-
-#+1 to add to either sttreak value
-
-#is random.choice being stored accidentally as a string- answer is yes
-#making sense of core logic, but clearly not the actual soluton
+    if any(heads_tails_list[i:i+6] == streak_H or heads_tails_list[i:i+6] == streak_T for i in range(len(heads_tails_list) - 5)):
+        number_of_streaks += 1
+print('Chance of streak: %s%%' % (number_of_streaks / 100))
